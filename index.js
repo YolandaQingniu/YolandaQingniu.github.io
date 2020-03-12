f74749618cadb4c42967e890d4e225819302cd3f
DonYau <DonYau@126.com>
2020-03-12 09:45:56 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,8 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish('_book',{
+    branch:"master",
+    repo:"https://github.com/YolandaQingniu/YolandaQingniu.github.io.git",
+}, function(err) {
+    console.log(err)
+});
