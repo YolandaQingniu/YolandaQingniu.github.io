73333e4a4d84428c8ce730f57945349fbab4ac8f
ch <chenhua@yolanda.hk>
2020-06-17 10:59:15 +0800

    Updates

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ ghpages.publish(
     repo: "https://github.com/YolandaQingniu/YolandaQingniu.github.io.git"
   },
   function(err) {
-    console.log("github更新" + err);
-  }
-);
-
-ghpages.publish(
-  "_book",
-  {
-    branch: "master",
-    repo: "https://gitee.com/yolandaqingniu/sdk-doc.git"
-  },
-  function(err) {
-    console.log("gitee更新" + err);
+    console.log(err);
   }
 );
